1b582144e95dd9000ec7a57a29e52dbc2128244d
Michael Aaron Murphy <mmstickman@gmail.com>
2018-10-07 14:05:09 -0600

    0.1.0: First crate release

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,36 @@
+//! Configurable parallel web crawler
+//! 
+//! # Example
+//! 
+//! ```rust,no_run
+//! extern crate url_crawler;
+//! use url_crawler::*;
+//! 
+//! /// Function for filtering content in the crawler before a HEAD request.
+//! /// 
+//! /// Only allow directory entries, and files that have the `deb` extension.
+//! fn apt_filter(url: &Url) -> bool {
+//!     let url = url.as_str();
+//!     url.ends_with("/") || url.ends_with(".deb")
+//! }
+//! 
+//! pub fn main() {
+//!     // Create a crawler designed to crawl the given website.
+//!     let crawler = Crawler::new("http://apt.pop-os.org/".into())
+//!         // Use four threads for fetching
+//!         .threads(4)
+//!         // Check if a URL matches this filter before performing a HEAD request on it.
+//!         .pre_fetch(apt_filter)
+//!         // Initialize the crawler and begin crawling. This returns immediately.
+//!         .crawl();
+//! 
+//!     // Process url entries as they become available
+//!     for file in crawler {
+//!         println!("{:#?}", file);
+//!     }
+//! }
+//! ```
+
 #[macro_use]
 extern crate bitflags;
 extern crate chrono;
@@ -8,17 +41,20 @@ extern crate failure_derive;
 extern crate reqwest;
 extern crate url_scraper;
 
+mod scraper;
+
 pub use reqwest::{Url, header};
-use reqwest::header::*;
 use chrono::{DateTime, FixedOffset};
 use crossbeam_channel as channel;
+use channel::Receiver;
 use reqwest::Client;
-use self::scraper::Scraper;
-use std::thread;
-use url_scraper::UrlScraper;
+use reqwest::header::*;
+use scraper::Scraper;
 use std::sync::Arc;
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
+use std::thread;
 use std::time::Duration;
+use url_scraper::UrlScraper;
 
 bitflags! {
     /// Flags for controlling the behavior of the crawler.
@@ -30,11 +66,14 @@ bitflags! {
     }
 }
 
-/// A configurable web crawler.
+/// A configurable parallel web crawler.
+/// 
+/// Crawling does not occur until this type is consumed by the `crawl` method.
 pub struct Crawler {
     url: String,
     threads: usize,
     flags: Flags,
+    errors: fn(Error) -> bool,
     pre_fetch: fn(&Url) -> bool,
     post_fetch: fn(&Url, &HeaderMap) -> bool,
 }
@@ -46,87 +85,95 @@ impl Crawler {
             url,
             threads: 4,
             flags: Flags::empty(),
+            errors: |_| true,
             pre_fetch: |_| true,
             post_fetch: |_, _| true,
         }
     }
 
-    /// Disables cross-domain crawling.
+    /// Set flags for configuring the crawler.
     pub fn flags(mut self, flags: Flags) -> Self {
         self.flags = flags;
         self
     }
 
     /// Specifies the number of fetcher threads to use.
+    /// 
+    /// # Notes
+    /// - If the input is `0`, `1` thread will be used.
+    /// - The default thread count is `4` when not using this method.
     pub fn threads(mut self, threads: usize) -> Self {
         self.threads = if threads == 0 { 1 } else { threads };
         self
     }
 
+    /// Allow the caller to handle errors.
+    /// 
+    /// # Notes
+    /// Returning `false` will stop the crawler.
+    pub fn errors(mut self, errors: fn(error: Error) -> bool) -> Self {
+        self.errors = errors;
+        self
+    }
+
     /// Enables filtering items based on their filename.
+    /// 
+    /// # Notes
+    /// Returning `false` will prevent the item from being fetched.
     pub fn pre_fetch(mut self, pre_fetch: fn(url: &Url) -> bool) -> Self {
         self.pre_fetch = pre_fetch;
         self
     }
 
     /// Enables filtering items based on their filename and requested headers.
+    /// 
+    /// # Notes
+    /// Returning `false` will prevent the item from being scraped / returned.
     pub fn post_fetch(mut self, post_fetch: fn(url: &Url, headers: &HeaderMap) -> bool) -> Self {
         self.post_fetch = post_fetch;
         self
     }
 
     /// Initializes the crawling, returning an iterator of discovered files.
-    pub fn crawl(self) -> impl Iterator<Item = UrlEntry> {
+    /// 
+    /// The crawler will continue to crawl in background threads even while the iterator
+    /// is not being pulled from.
+    pub fn crawl(self) -> CrawlIter {
         let client_ = Arc::new(Client::new());
 
         let threads = self.threads;
         let pre_fetch = self.pre_fetch;
         let post_fetch = self.post_fetch;
+        let errors = self.errors;
         let flags = self.flags;
         let (scraper_tx, scraper_rx) = channel::unbounded::<String>();
         let (fetcher_tx, fetcher_rx) = channel::bounded::<Url>(threads * 4);
-        let (compare_tx, compare_rx) = channel::bounded::<UrlEntry>(threads * 4);
-        let state = Arc::new(AtomicUsize::new(4));
+        let (output_tx, output_rx) = channel::bounded::<UrlEntry>(threads * 4);
+        let state = Arc::new(AtomicUsize::new(0));
+        let kill = Arc::new(AtomicBool::new(false));
         scraper_tx.send(self.url);
-
-        // Thread for scraping urls and avoiding repeats.
-        let status = state.clone();
-        let client = client_.clone();
-        thread::spawn(move || {
-            let mut visited = Vec::new();
-            loop {
-                let url: String = match scraper_rx.try_recv() {
-                    Some(url) => url,
-                    None => {
-                        if status.load(Ordering::SeqCst) == threads && scraper_rx.is_empty() {
-                            break
-                        }
-
-                        thread::sleep(Duration::from_millis(1));
-                        continue
-                    }
-                };
-
-                if let Ok(scraper) = UrlScraper::new_with_client(&url, &client) {
-                    for url in Scraper::new(scraper.into_iter(), &url, &mut visited, flags) {
-                        fetcher_tx.send(url);
-                    }
-                }
-            }
-        });
 
         for _ in 0..threads {
             let fetcher = fetcher_rx.clone();
             let client = client_.clone();
             let scraper_tx = scraper_tx.clone();
-            let compare_tx = compare_tx.clone();
+            let output_tx = output_tx.clone();
             let status = state.clone();
+            let kill = kill.clone();
             thread::spawn(move || {
+                status.fetch_add(1, Ordering::SeqCst);
                 for url in fetcher {
                     status.fetch_sub(1, Ordering::SeqCst);
+                    if kill.load(Ordering::SeqCst) {
+                        break
+                    }
+
                     let head = match client.head(url.clone()).send() {
                         Ok(head) => head,
-                        _ => {
+                        Err(why) => {
+                            if ! errors(why.into()) {
+                                kill.store(true, Ordering::SeqCst);
+                            }
                             status.fetch_add(1, Ordering::SeqCst);
                             continue
                         }
@@ -147,7 +194,7 @@ impl Crawler {
                     if let Some(content_type) = head.headers().get(CONTENT_TYPE).and_then(|c| c.to_str().ok()) {
                         if content_type.starts_with("text/html") {
                             scraper_tx.send(url.to_string());
-                            compare_tx.send(UrlEntry::Html { url });
+                            output_tx.send(UrlEntry::Html { url });
                         } else {
                             let length: u64 = headers.get(CONTENT_LENGTH)
                                 .and_then(|c| c.to_str().ok())
@@ -158,7 +205,7 @@ impl Crawler {
                                 .and_then(|c| c.to_str().ok())
                                 .and_then(|c| DateTime::parse_from_rfc2822(c).ok());
 
-                            compare_tx.send(UrlEntry::File { url, length, modified });
+                            output_tx.send(UrlEntry::File { url, length, modified });
                         }
                     }
 
@@ -167,7 +214,65 @@ impl Crawler {
             });
         }
 
-        compare_rx.into_iter()
+        // Thread for scraping urls and avoiding repeats.
+        let state_ = state.clone();
+        let client = client_.clone();
+        let kill_ = kill.clone();
+        thread::spawn(move || {
+            let mut visited = Vec::new();
+            let jobs_complete = || {
+                state_.load(Ordering::SeqCst) == threads
+                    && scraper_rx.is_empty()
+                    && fetcher_tx.is_empty()
+            };
+
+            while ! kill_.load(Ordering::SeqCst) {
+                let url: String = match scraper_rx.try_recv() {
+                    Some(url) => url,
+                    None => {
+                        if jobs_complete() { break }
+                        thread::sleep(Duration::from_millis(1));
+                        continue
+                    }
+                };
+
+                match UrlScraper::new_with_client(&url, &client) {
+                    Ok(scraper) => for url in Scraper::new(scraper.into_iter(), &url, &mut visited, flags) {
+                        fetcher_tx.send(url);
+                    }
+                    Err(why) => if ! errors(why.into()) {
+                        kill_.store(true, Ordering::SeqCst);
+                    }
+                }
+            }
+        });
+
+        CrawlIter {
+            recv: output_rx,
+            kill
+        }
+    }
+}
+
+/// Iterator that returns fetched `UrlEntry` items.
+/// 
+/// On drop, the crawler's threads will be killed.
+pub struct CrawlIter {
+    recv: Receiver<UrlEntry>,
+    kill: Arc<AtomicBool>,
+}
+
+impl Iterator for CrawlIter {
+    type Item = UrlEntry;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.recv.next()
+    }
+}
+
+impl Drop for CrawlIter {
+    fn drop(&mut self) {
+        self.kill.store(true, Ordering::SeqCst);
     }
 }
 
@@ -186,60 +291,6 @@ pub fn filename_from_url(url: &str) -> &str {
         url
     } else {
         &url[url.rfind('/').unwrap_or(0)+1..]
-    }
-}
-
-mod scraper {
-    use reqwest::Url;
-    use std::collections::hash_map::DefaultHasher;
-    use std::hash::{Hash, Hasher};
-    use url_scraper::UrlIter;
-    use super::Flags;
-
-    pub struct Scraper<'a> {
-        iter: UrlIter<'a, 'a>,
-        url: Url,
-        visited: &'a mut Vec<u64>,
-        flags: Flags
-    }
-
-    impl<'a> Scraper<'a> {
-        pub fn new(iter: UrlIter<'a, 'a>, url: &'a str, visited: &'a mut Vec<u64>, flags: Flags) -> Self {
-            Self { iter, url: Url::parse(url).unwrap(), visited, flags }
-        }
-    }
-
-    impl<'a> Iterator for Scraper<'a> {
-        type Item = Url;
-
-        fn next(&mut self) -> Option<Self::Item> {
-            for (_, url) in &mut self.iter {
-                if ! self.flags.contains(Flags::CROSS_DOMAIN) {
-                    if url.domain() != self.url.domain() {
-                        continue
-                    }
-                }
-
-                if ! self.flags.contains(Flags::CROSS_DIR) {
-                    if ! url.path().starts_with(self.url.path()) {
-                        continue
-                    }
-                }
-
-                let mut hasher = DefaultHasher::new();
-                url.as_str().hash(&mut hasher);
-                let hash = hasher.finish();
-
-                if self.visited.contains(&hash) {
-                    continue
-                }
-                self.visited.push(hash);
-
-                return Some(url);
-            }
-
-            None
-        }
     }
 }
 
